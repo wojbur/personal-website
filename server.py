dbd29884c198fa456271c16fff52467aa61c48ad
Wojciech Burman <wojbur94@gmail.com>
2022-10-31 12:17:33 +0100

    add page template, fix broken links, start server.py file

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)

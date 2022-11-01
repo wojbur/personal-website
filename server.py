d41d74ad4dab5ab9fc56abd2d32a802d4d7a547d
Wojciech Burman <wojbur94@gmail.com>
2022-11-01 15:53:44 +0100

    add submit form func

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +7,19 @@ app = Flask(__name__)
 def home_page():
     return render_template("index.html")
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def sumbit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect("thankyou.html")
+    else:
+        return 'somthing went wrong'
